b320cd859b79b3402ca2d74cb7df17b277fc9be5
yuichi tanaka <u1tnk1123@gmail.com>
2014-05-21 11:17:47 +0900

    gmailを使って送信したところ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -34,4 +34,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+  config.action_mailer.default_url_options = { :host => "localhost", :port => 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "u1tnk1123@gmail.com",
+    :password => "efdascgxiwbhhjfp",
+    :authentication => 'plain',
+    :enable_starttls_auto => true,
+  }
 end
